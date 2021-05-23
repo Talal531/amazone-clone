e1ceadb790a4aa3e98ce37d8c1d3550069543e80
Talal531 <mtm_531@hotmail.com>
2021-05-24 01:41:21 +0500

    build header component

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="flex flex-grow items-center mt-2 sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit={"contain"}
+            className="cursor-pointer"
+          />
+        </div>
+
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello, Talal Malick</p>
+            <p className="font-extrabold md:text-sm">Account & List</p>
+          </div>
+
+          <div className="link">
+            <p>Refunds</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-12" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
 
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Amazon Clone</title>
       </Head>
+      <Header />
     </div>
   );
 }
